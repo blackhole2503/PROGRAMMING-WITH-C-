e29910756bf18fb3fb344b2dd095973b05a0aba4
Blackhole2503 <82560504+blackhole2503@users.noreply.github.com>
2022-02-15 21:28:27 +0530

    Add files via upload

---
+++ sizeofcontainer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+
+cout<<"size of int is : "<<sizeof(char)<<" bytes"<<endl;
+cout<<"size of int is : "<<sizeof(int)<<" bytes"<<endl;
+cout<<"size of int is : "<<sizeof(float)<<" bytes"<<endl;
+cout<<"size of int is : "<<sizeof(double)<<" bytes"<<endl;
+ 
+return 0;
+}
