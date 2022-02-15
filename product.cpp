cdce4621977d3a9cbfa023677fdf48c2867c3100
Blackhole2503 <82560504+blackhole2503@users.noreply.github.com>
2022-02-15 21:33:58 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+
+double x,y;     
+cout<<"ENTER TWO NUMBERS :"<<endl;
+cin>>x;
+cin>>y;
+cout<<"MULTIPLICATION OF THE GIVEN NUMBER'S IS : "<<x*y<<endl;
+ 
+return 0;
+}
